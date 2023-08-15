8e2ec803fc94adca06c58429aa3df51097e5fb5c
Sheflex4real <sheriph001@gmail.com>
2023-08-15 12:43:17 +0100

    added active class

--- public/index.js
+++ public/index.js
@@ -1,19 +1,42 @@
+// const { set } = require("express/lib/response")
 
 const clickIcon = document.querySelector(".hamburger")
 const navToggle = document.querySelector('.nav-bar')
 const slideShow = document.querySelector('.container')
+const navLinks = document.querySelectorAll("li");
+
+
+
 function changeHere(e){
   const myTarget = e.target
   if(myTarget.nextElementSibling.classList.contains("hidden")){
     navToggle.classList.remove('hidden')
     slideShow.style.marginTop = "250px"
+    navToggle.classList.add("")
+
   }
   else{
     navToggle.classList.add('hidden')
     slideShow.style.marginTop =""
+    navToggle.classList.add("nav")
   }
-
 }
+
+const setActiveLink = (e)=>{
+  [].forEach.call(navLinks,(link)=>{
+    console.log(link);
+    link.classList.remove("active")
+  } );
+e.target.classList.add("active")
+}
+
+
+navLinks.forEach(link =>{
+link.addEventListener("click", setActiveLink)
+
+})
+
+
 
 
 let i = 0
@@ -40,7 +63,7 @@ setTimeout("slideImage()", time)
 
 window.onload = slideImage
 
-
+// window.onload = setActiveLink
 
 
 
